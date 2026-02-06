e133f2edd1c0fcf62b56623d777406417cf73b6b
Carlos <carlos.rodriguez1@alumnos.uneatlantico.es>
2026-02-06 11:18:29 +0100

    Defino e implemento una manera de representar la información a almacenar

---
+++ evaluaciones/retos/reto002/Edlin.java
@@ -0,0 +1,15 @@
+public class Edlin {
+    private String[] lineas = new String[10];
+
+    public Edlin() {
+        for (int i = 0; i < 10; i++) {
+            lineas[i] = "";
+        }
+    }
+
+    public void mostrar() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println((i + 1) + ": " + lineas[i]);
+        }
+    }
+}
